aa3697402cf750b961279ea5af4eab2f112e9180
Tulip Blossom <tulilirockz@outlook.com>
2025-01-07 01:33:36 -0300

    chore: shellcheck cleanups for all files + remove unnecessary override

--- system_files/usr/share/ublue-os/user-setup.hooks.d/10-theming.sh
+++ system_files/usr/share/ublue-os/user-setup.hooks.d/10-theming.sh
@@ -6,20 +6,20 @@ PTYXIS_THEME_DIR="/etc/skel/.local/share/org.gnome.Ptyxis/palettes"
 PTYXIS_DIR="$HOME/.local/share/org.gnome.Ptyxis/palettes"
 mkdir -p "$PTYXIS_DIR"
 if [[ ! -f "$PTYXIS_DIR/catppuccin-dynamic.palette" ]]; then
-  cp "$PTYXIS_THEME_DIR/catppuccin-dynamic.palette" "$PTYXIS_DIR/catppuccin-dynamic.palette"
+	cp "$PTYXIS_THEME_DIR/catppuccin-dynamic.palette" "$PTYXIS_DIR/catppuccin-dynamic.palette"
 fi
 
-if [[ ":Framework:" =~ ":$VEN_ID:" ]]; then
-  if [[ ! -f "$UBLUE_CONFIG_DIR/framework-initialized" ]]; then
-    echo 'Setting Framework logo menu'
-    dconf write /org/gnome/shell/extensions/Logo-menu/symbolic-icon true
-    dconf write /org/gnome/shell/extensions/Logo-menu/menu-button-icon-image 31
-    echo 'Setting touch scroll type'
-    dconf write /org/gnome/desktop/peripherals/mouse/natural-scroll true
-    if [[ $SYS_ID == "Laptop ("* ]]; then
-      echo 'Applying font fix for Framework 13'
-      dconf write /org/gnome/desktop/interface/text-scaling-factor 1.25
-    fi
-    touch "$UBLUE_CONFIG_DIR/framework-initialized"
-  fi
+if [[ ":Framework:" =~ :$VEN_ID: ]]; then
+	if [[ ! -f "$UBLUE_CONFIG_DIR/framework-initialized" ]]; then
+		echo 'Setting Framework logo menu'
+		dconf write /org/gnome/shell/extensions/Logo-menu/symbolic-icon true
+		dconf write /org/gnome/shell/extensions/Logo-menu/menu-button-icon-image 31
+		echo 'Setting touch scroll type'
+		dconf write /org/gnome/desktop/peripherals/mouse/natural-scroll true
+		if [[ $SYS_ID == "Laptop ("* ]]; then
+			echo 'Applying font fix for Framework 13'
+			dconf write /org/gnome/desktop/interface/text-scaling-factor 1.25
+		fi
+		touch "$UBLUE_CONFIG_DIR/framework-initialized"
+	fi
 fi
