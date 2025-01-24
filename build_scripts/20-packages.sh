89cc7e8d6c57344b35ad16749a8fb64db410caa3
Tulip Blossom <tulilirockz@outlook.com>
2025-01-24 00:51:26 -0300

    fix: re sort packages and make sure the old extensions app isnt installed

--- build_scripts/20-packages.sh
+++ build_scripts/20-packages.sh
@@ -7,16 +7,17 @@ MAJOR_VERSION="$(sh -c '. /usr/lib/os-release ; echo $VERSION_ID')"
 dnf -y remove \
 	subscription-manager
 
+
 dnf -y install \
+	-x gnome-extensions-app \
 	distrobox \
-        distribution-gpg-keys \
-	fastfetch \
-	fpaste \
-	gnome-extensions-app \
-	gnome-shell-extension-{appindicator,dash-to-dock,blur-my-shell} \
-        just \
-	powertop \ 
-	tuned-ppd \
+ 	distribution-gpg-keys \
+  	fastfetch \
+   	fpaste \
+ 	gnome-shell-extension-{appindicator,dash-to-dock,blur-my-shell} \
+  	just \
+   	powertop \
+	tuned-ppd
 
 # Everything that depends on external repositories should be after this.
 # Make sure to set them as disabled and enable them only when you are going to use their packages.
