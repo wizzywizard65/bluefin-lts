c9167a0a9d1bc013e7be5ee0729298d7a53d80c1
Tulip Blossom <tulilirockz@outlook.com>
2025-01-29 01:31:51 -0300

    feat: move everything to ublue-os/bluefin-lts

--- build_scripts/00-workarounds.sh
+++ build_scripts/00-workarounds.sh
@@ -4,7 +4,7 @@ set -xeuo pipefail
 
 # This is a bucket list. We want to not have anything in this file at all.
 
-# See https://github.com/centos-workstation/achillobator/issues/3
+# See https://github.com/ublue-os/bluefin-lts/issues/3
 mkdir -p /var/roothome
 chmod 0700 /var/roothome
 
