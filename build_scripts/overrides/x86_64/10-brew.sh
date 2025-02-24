28572c3a4b52de0e69feeb7bd1c3a8176c464ff1
Jorge O. Castro <jorge.castro@gmail.com>
2025-02-24 15:57:37 -0500

    fix: update brew copr (#327)

--- build_scripts/overrides/x86_64/10-brew.sh
+++ build_scripts/overrides/x86_64/10-brew.sh
@@ -2,7 +2,7 @@
 
 set -xeuo pipefail
 
-dnf -y --enablerepo copr:copr.fedorainfracloud.org:ublue-os:staging install ublue-brew
+dnf -y --enablerepo copr:copr.fedorainfracloud.org:ublue-os:packages install ublue-brew
 
 # Forcefully enable brew setup since the preset doesnt seem to work?
 systemctl enable brew-setup.service
