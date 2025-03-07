d7c48ba36a4bc4b22116814f4f65ef68237702f4
Kyle Gospodnetich <me@kylegospodneti.ch>
2025-03-07 08:09:54 -0800

    feat: Add Ampere logo setter (#368)

--- system_files/usr/share/ublue-os/user-setup.hooks.d/10-theming.sh
+++ system_files/usr/share/ublue-os/user-setup.hooks.d/10-theming.sh
@@ -19,3 +19,11 @@ if [[ ":Framework:" =~ :$VEN_ID: ]]; then
 		dconf write /org/gnome/desktop/interface/text-scaling-factor 1.25
 	fi
 fi
+
+SYS_ID="$(cat /sys/devices/virtual/dmi/id/product_name)"
+
+if [[ ":Thelio Astra:" =~ :$SYS_ID: ]]; then
+	echo 'Setting Ampere Logo'
+ 	dconf write /org/gnome/shell/extensions/Logo-menu/symbolic-icon true
+	dconf write /org/gnome/shell/extensions/Logo-menu/menu-button-icon-image 32
+ fi
