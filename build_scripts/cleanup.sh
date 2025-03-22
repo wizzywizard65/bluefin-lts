ef9735de72e62c0159dcaa4fc54cd49de3250c9b
James Reilly <jreilly1821@gmail.com>
2025-03-23 01:48:32 +0530

    fix: /usr/local not writeable (#413)
    Fixes #412 with @imbev 's fix
    
    ---------
    
    Co-authored-by: Jorge O. Castro <jorge.castro@gmail.com>

--- build_scripts/cleanup.sh
+++ build_scripts/cleanup.sh
@@ -20,6 +20,10 @@ dnf clean all
 rm -rf /.gitkeep /var /boot
 mkdir -p /boot /var
 
+# Make /usr/local writeable
+mv /usr/local /var/usrlocal
+ln -s /var/usrlocal /usr/local
+
 # Set file to globally readable
 # FIXME: This should not be necessary, needs to be cleaned up somewhere else
 chmod 644 "/usr/share/ublue-os/image-info.json"
