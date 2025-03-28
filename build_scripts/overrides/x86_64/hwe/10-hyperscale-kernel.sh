428aad031befd3708c1cb3b30ddddfb42566809a
Tulip Blossom <tulilirockz@outlook.com>
2025-03-28 11:54:41 -0300

    fix(hwe): add ostree module to initramfs so we can actually boot

--- build_scripts/overrides/x86_64/hwe/10-hyperscale-kernel.sh
+++ build_scripts/overrides/x86_64/hwe/10-hyperscale-kernel.sh
@@ -12,5 +12,5 @@ dnf versionlock add kernel kernel-devel kernel-devel-matched kernel-core kernel-
 
 # Only necessary when not building with Nvidia
 KERNEL_SUFFIX=""
-QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
-/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible --zstd -v -f
+QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//' | tail -n 1)"
+/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible --zstd -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
