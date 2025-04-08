a55bebbee347019355346526f55b47bf7e0bd556
Alex Iribarren <alex.iribarren@gmail.com>
2025-04-08 22:18:43 +0200

    fix: improve parsing of MAJOR_VERSION_NUMBER (#430)
    cause you never really know what someone's going to put in there, do
    you? ;)

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -10,7 +10,7 @@ set -eo pipefail
 
 CONTEXT_PATH="$(realpath "$(dirname "$0")/..")" # should return /run/context
 BUILD_SCRIPTS_PATH="$(realpath "$(dirname "$0")")"
-MAJOR_VERSION_NUMBER="$(sh -c '. /usr/lib/os-release ; echo $VERSION_ID')"
+MAJOR_VERSION_NUMBER="$(sh -c '. /usr/lib/os-release ; echo ${VERSION_ID%.*}')"
 SCRIPTS_PATH="$(realpath "$(dirname "$0")/scripts")"
 export SCRIPTS_PATH
 export MAJOR_VERSION_NUMBER
