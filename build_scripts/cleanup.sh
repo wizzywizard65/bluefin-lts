3539102ed2695ae524dec75f5fce530bc104d89f
Tulip Blossom <tulilirockz@outlook.com>
2025-04-14 09:59:26 -0300

    fix: permissions for image-info json file (#441)
    Co-authored-by: James Reilly <jreilly1821@gmail.com>

--- build_scripts/cleanup.sh
+++ build_scripts/cleanup.sh
@@ -19,5 +19,9 @@ mkdir -p /var /boot
 # Make /usr/local writeable
 ln -s /var/usrlocal /usr/local
 
+# We need this else anything accessing image-info fails
+# FIXME: Figure out why this doesnt have the right permissions by default
+chmod 644 /usr/share/ublue-os/image-info.json
+
 # FIXME: use --fix option once https://github.com/containers/bootc/pull/1152 is merged
 bootc container lint --fatal-warnings || true
