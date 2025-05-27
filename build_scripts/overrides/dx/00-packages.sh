74a89805cfaaa749bd015da98b33c28611c7378f
Rich Renomeron <rrenomeron+github@gmail.com>
2025-05-27 08:28:35 -0400

    fix: working vscode install GPG check (#495)
    This patch allows for install of VSCode while still checking the GPG
    signatures on the packages. Apparently Microsoft is (still!) using SHA-1
    to sign packages, and that's no longer allowed by default in EL 10. So
    we allow "the old ways" temporarily so we can get the package installed.
    
    One side effect is that RPM will gladly tell you all about failed
    signatures when you run ``rpm -q code`` or ``rpm -qa``. Unless there are
    other consequences I don't know about, that seems like a fair tradeoff
    for the Warm Security Blanket (pun intended) of validating the GPG
    signature on the package.
    
    See
    https://discussion.fedoraproject.org/t/rejected-third-party-repository-keys/144806/6
    for details.

--- build_scripts/overrides/dx/00-packages.sh
+++ build_scripts/overrides/dx/00-packages.sh
@@ -8,7 +8,10 @@ dnf install -y \
 # VSCode on the base image!
 dnf config-manager --add-repo "https://packages.microsoft.com/yumrepos/vscode"
 dnf config-manager --set-disabled packages.microsoft.com_yumrepos_vscode
-dnf -y --enablerepo packages.microsoft.com_yumrepos_vscode --nogpgcheck  install code
+update-crypto-policies --set LEGACY
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+dnf -y --enablerepo packages.microsoft.com_yumrepos_vscode install code
+update-crypto-policies --set DEFAULT
 
 dnf config-manager --add-repo "https://download.docker.com/linux/centos/docker-ce.repo"
 dnf config-manager --set-disabled docker-ce-stable
