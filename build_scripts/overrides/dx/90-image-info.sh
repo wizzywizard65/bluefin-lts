cae568de764e8b4050dd7965b53371e7c9de2a33
Rich Renomeron <rrenomeron+github@gmail.com>
2026-01-03 13:02:40 -0700

    fix(dx): identify as dx in imageinfo (#958)
    Fixes image info generation on DX.

--- build_scripts/overrides/dx/90-image-info.sh
+++ build_scripts/overrides/dx/90-image-info.sh
@@ -3,5 +3,9 @@
 set -xeuo pipefail
 
 FLAVOR="dx"
+IMAGE_NAME="bluefin-${FLAVOR}"
+IMAGE_REF="ostree-image-signed:docker://ghcr.io/ublue-os/bluefin-${FLAVOR}"
 export FLAVOR
+export IMAGE_NAME
+export IMAGE_REF
 "${SCRIPTS_PATH}/image-info-set"
